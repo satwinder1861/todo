380252949616422c5ae12435a04a535d3b00139e
satwinder <satwinder47@outlook.com>
2022-02-14 21:57:17 -0500

    todo app

--- src/Todos/thunk.js
+++ src/Todos/thunk.js
@@ -1,10 +1,11 @@
+// redux thunks for side effects
 import {createTodo,removeTodo,markTodosAsCompleted, loadTodosInProgress,loadTodosSuccess,loadTodosFailure } from "./action";
 
 
 export const loadTodos = () => async(dispatch,getState)=>{
 try{
     dispatch(loadTodosInProgress());
-const response  = await fetch ('http://localhost:8080/todos');
+const response  = await fetch ('https://satwindertodo.herokuapp.com/todos');
 const todos = await response.json();
 console.log(todos);
 dispatch(loadTodosSuccess(todos));
@@ -16,7 +17,7 @@ dispatch(loadTodosSuccess(todos));
 export const addTodoRequest = text => async dispatch => {
   try{
     const body = JSON.stringify({text});
-  const response = await fetch('http://localhost:8080/todos',{
+  const response = await fetch('https://satwindertodo.herokuapp.com/todos',{
       headers:{
 'Content-Type' : 'application/json',
   },
@@ -33,7 +34,7 @@ dispatch(createTodo(todo));
 
 export const removeTodoRequest = id => async dispatch => {
   try {
-    const response = await fetch(`http://localhost:8080/todos/${id}`,{
+    const response = await fetch(`https://satwindertodo.herokuapp.com/todos/${id}`,{
   method:'delete',
   });
 
@@ -46,7 +47,7 @@ export const removeTodoRequest = id => async dispatch => {
 
 export const markTodosAsCompletedRequest = id => async dispatch =>{
 try {
-  const response = await fetch(`http://localhost:8080/todos/${id}/completed`,{
+  const response = await fetch(`https://satwindertodo.herokuapp.com/todos/${id}/completed`,{
 method:'post'
 });
 const updatedTodo = await response.json();
